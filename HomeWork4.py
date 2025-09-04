ecb20c02c51af42cc070d7fa1a417db10e617574
Misha Polunin <polunin1986@gmail.com>
2025-09-04 12:01:47 +0300

    - remade homework4

--- HomeWork4.py
+++ HomeWork4.py
@@ -2,24 +2,16 @@ numberOne = int(input("Enter first number "))
 numberTwo = int(input("Enter second number "))
 operation = input("Choose Operation: + as addition, - as subtraction, * as multiplication, / as divide: ")
 
-if operation == "+" or operation == "-" or operation == "*" or operation ==  "/":
-    if  operation == "+":
-        addition = numberOne + numberTwo
-        print("Result of addition: ", addition)
-
-    if operation == "-":
-        subtraction = numberOne - numberTwo
-        print("Result of subtraction: ",subtraction)
-
-    if operation == "*":
-        multiplication = numberOne * numberTwo
-        print("Result of multiplication: ",multiplication)
-
-    if operation == "/":
-        if numberTwo == 0:
-            print("Error: can't be divided by zero")
-        else:
-            divide = numberOne / numberTwo
-            print("Result of divide:", divide)
+if  operation == "+":
+    print("Result of addition: ", numberOne + numberTwo)
+elif operation == "-":
+    print("Result of subtraction: ",numberOne - numberTwo)
+elif operation == "*":
+    print("Result of multiplication: ",numberOne * numberTwo)
+elif operation == "/":
+    if numberTwo == 0:
+        print("Error: can't be divided by zero")
+    else:
+        print("Result of divide:", numberOne / numberTwo)
 else:
     print("Calculator doesn't work with another operations")
