ca3edc9ab79d3937bbf124878c9fe9c6fc95b265
Misha Polunin <polunin1986@gmail.com>
2025-09-08 10:33:43 +0300

    - fixed homework 8

--- HomeWork8.py
+++ HomeWork8.py
@@ -1,6 +1,10 @@
 numbers = [0, 1, 7, 2, 4, 8]
 result = 0
 
-for i in range(0, len(numbers), 2):
-    result += numbers[i] * numbers[-1]
-print(result)
+if numbers == []:
+    print("[0]")
+else:
+    for i in range(0, len(numbers), 2):
+        result += numbers[i]
+    result *= numbers[-1]
+    print(result)
