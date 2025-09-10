0eccb033f2a289cddf24eb35fe9c2f3f8e8bb8cb
Misha Polunin <polunin1986@gmail.com>
2025-09-10 22:10:04 +0300

    - HomeWork11.py

---
+++ HomeWork11.py
@@ -0,0 +1,23 @@
+while True:
+    numberOne = int(input("Enter first number "))
+    numberTwo = int(input("Enter second number "))
+    operation = input("Choose Operation: + as addition, - as subtraction, * as multiplication, / as divide: ")
+
+    if operation == "+":
+        print("Result of addition: ", numberOne + numberTwo)
+    elif operation == "-":
+        print("Result of subtraction: ", numberOne - numberTwo)
+    elif operation == "*":
+        print("Result of multiplication: ", numberOne * numberTwo)
+    elif operation == "/":
+        if numberTwo == 0:
+            print("Error: can't be divided by zero")
+        else:
+            print("Result of divide:", numberOne / numberTwo)
+    else:
+        print("Calculator doesn't work with another operations")
+
+    question = input("Do you want to continue? (y/n): ").lower()
+    if question != "y":
+        print("Calculator stopped!")
+        break
