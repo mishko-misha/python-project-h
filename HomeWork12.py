e4c69bd0e87bbc2aa29512cbffbe9222e226bf50
Misha Polunin <polunin1986@gmail.com>
2025-09-14 15:42:14 +0300

    - added HomeWork12.py

--- HomeWork12.py
+++ HomeWork12.py
@@ -2,5 +2,5 @@ text_for_hashtag = input("Please enter your text to generate to hashtag: ")
 
 text_title = text_for_hashtag.title()
 text_with_out_punctuation_gaps = ''.join(char for char in text_title if char.isalnum())
-hashtag = '#' + text_with_out_punctuation_gaps[:140]
-print(f"Text is generated to new hashtag: {hashtag}")
+hashtag = text_with_out_punctuation_gaps[:140]
+print(f"Text is generated to new hashtag: #{hashtag}")
