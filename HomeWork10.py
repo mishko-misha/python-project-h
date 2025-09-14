0e29e4e3dc10ca25d16c3639ed12c58bf4573010
Misha Polunin <polunin1986@gmail.com>
2025-09-14 14:09:07 +0300

    - HomeWork10.py

--- HomeWork10.py
+++ HomeWork10.py
@@ -8,48 +8,34 @@ keyword_kwlist = ['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'awai
                   'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from',
                   'global', 'if', 'import', 'in', 'is', 'match', 'nonlocal', 'not', 'or', 'pass', 'raise',
                   'return', 'try', 'while', 'with', 'yield']
-if "___" in name:
-    print(False)  # __ => False
-elif "__" in name:
-    print(False)  # ___ => False
-if "_" in name:
-    parts_of_name = name.split("_")
-    if (len(parts_of_name) == 2 and all(part.isalpha() for part in parts_of_name)
-            and parts_of_name[0].istitle() and parts_of_name[1].islower()):
-        print(False)  # Get_value => False
-    elif (len(parts_of_name) == 2 and all(part.isalpha() for part in parts_of_name)
-          and parts_of_name[0].islower() and parts_of_name[1].istitle()):
-        print(False)  # get_Value => False
-    elif len(parts_of_name) == 2 and all(part.isalpha() and part.islower() for part in parts_of_name):
-        print(True)  # _ => True and get_value => True
-    elif len(parts_of_name) == 4 and all(part.isalpha() and part.islower() for part in parts_of_name):
-        print(True)  # some_super_puper_value => True
 
-if name[0].isdigit():
-    print(False)  # 3m => False
-elif name[-1].isdigit():
-    print(True)  # m3 => True
-
-if " " in name:
-    parts_of_name = name.split(" ")
-    if len(parts_of_name) == 2 and all(part.isalpha() for part in parts_of_name):
-        print(False)  # get value => False
-elif name.isalpha() and not name[1:]:
+if name.count("_") == len(name) and len(name) > 1:
+    print(False)  # __ => False ___ => False
+elif len(name) == 1 and name.isalpha():
     print(True)  # x => True
-
-if "_" not in name:
-    for i, letter in enumerate(name[1:], start=1):
-        if letter.isalpha() and letter.isupper():
+elif name[0].isdigit():
+    print(False)  # 3m => False
+elif "_" in name:
+    parts_of_name = name.split("_")
+    first_name = parts_of_name[0]
+    last_name = parts_of_name[1]
+    if all(part.isalpha() and part.islower() for part in parts_of_name):
+        print(True)  # _ => True and get_value => True and some_super_puper_value => True
+    elif first_name[0].istitle() and last_name.islower() and all(part.isalpha() for part in parts_of_name):
+        print(False)  # Get_value => False
+    elif first_name.islower() and last_name.istitle() and all(part.isalpha() for part in parts_of_name):
+        print(False)  # get_Value => False
+    elif first_name.isalpha() and last_name[-1].isdigit():
+        print(True)  # m3 => True
+elif " " in name:
+    print(False)  # get value => False
+elif name.isalpha():
+    for i in range(1, len(name)):
+        if name[i].isupper():
             print(False)  # getValue => False
-
-for char in string_punctuation:
-    if char in name:
-        parts_of_name = name.split(char)
-        if len(parts_of_name) == 2 and all(part.isalpha() for part in parts_of_name):
-            print(False)  # get!value => False
-
-if name in keyword_kwlist:
+elif any(char in name for char in string_punctuation):
+    print(False)  # get!value => False
+elif name in keyword_kwlist:
     print(False)  # assert => False
-#else doesn't work
 else:
     print(True)  # assert_exception => True
