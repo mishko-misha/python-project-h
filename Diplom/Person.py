c040f5a589acdc7be8f5f04647bb524df1d5422a
Misha Polunin <polunin1986@gmail.com>
2025-10-10 21:52:57 +0300

    - Person.py

--- Diplom/Person.py
+++ Diplom/Person.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+
+
 class Person:
 
     def __init__(self, first_name, middle_name=None, last_name=None, birth_date=None, death_date=None, gender=None):
@@ -6,28 +9,50 @@ class Person:
         self.last_name = last_name.capitalize()
         self.birth_date = birth_date
         self.death_date = death_date
-        self.gender = gender.lower() if gender in ('м', 'ж') else 'невідомо'
+        self.gender = gender.lower()
 
     @staticmethod
     def _get_date(text):
-        for char in [' ', '/','-']:
-            text = text.replace(char, ' ')
-        parts = text.split()
-
-        if len(parts) != 3:
-            raise ValueError("Невірний формат дати,повинно бути 3 числа).")
-        day = parts[0].zfill(2)
-        month = parts[1].zfill(2)
-        year = parts[2].zfill(2)
-
-        return f'{day}.{month}.{year}'
+        for format_date in ('%d.%m.%Y', '%d %m %Y', '%d/%m/%Y', '%d-%m-%Y'):
+            try:
+                return datetime.strptime(text.strip(), format_date).date()
+            except ValueError:
+                continue
+        raise ValueError('f"Неправильний формат дати: {date_str}"')
 
     def get_age(self):
-        pass
+        birth = self._get_date(self.birth_date)
+        death = self._get_date(self.death_date)
+        end_date = death or datetime.today().date()
+
+        age = end_date.year - birth.year
+        if (end_date.month, end_date.day) < (birth.month, birth.day):
+            age -= 1
+        return age
 
     def get_gender(self):
-        return 'чоловік' if self.gender == 'м' else 'жінка' if self.gender == 'ж' else 'невідомо'
+        return 'чоловік' if self.gender in ('ч', 'м') else 'жінка' if self.gender == 'ж' else 'невідомо'
 
     def __str__(self):
-        info = f'Name: {self.first_name}, Middle name: {self.middle_name}, Last Name: {self.last_name}'
-        return info
+        info = [f'Ім\'я: {self.first_name}']
+
+        if self.middle_name:
+            info.append(f' По-батькові: {self.middle_name}')
+        if self.last_name:
+            info.append(f' Прізвище: {self.last_name}')
+        if self.get_age():
+            info.append(f'Вік: {self.get_age()}')
+
+        birth = self._get_date(self.birth_date)
+        death = self._get_date(self.death_date)
+
+        if birth:
+            gender_check = 'Народився:' if self.gender in ('ч',
+                                                           'м') else 'Народилася:' if self.gender == 'ж' else 'Народився/лася: '
+            info.append(f'{gender_check}{birth}')
+        if death:
+            gender_check = 'Помер' if self.gender in ('ч', 'м') else 'Померла' if self.gender == 'ж' else 'Помер/ла: '
+            info.append(f' {gender_check}: {death}')
+        if self.gender in ('ч', 'м', 'ж'):
+            info.append(f' Стать:{self.get_gender()}')
+        return ' '.join(info)
